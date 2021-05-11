029c97e8fef22f12dbdcd93f08f689b96e2124eb
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-05-11 11:37:57 +0545

    minor changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # js-carousel
 
 ### usage: (v1)
-1. image should be of 800px x 600px resolution
+1. image should be of 600px x 400px resolution
 2. carousel.css should be imported
--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@
 js-carousel
 usage: (v1)
 
-  1. image should be of 800px x 600px resolution
+  1. image should be of 600px x 400px resolution
   2. carousel.css should be imported
 */
 
